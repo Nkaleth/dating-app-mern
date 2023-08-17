7ebec802c8d6eb22c3b9322b966fa552de74727d
Nkaleth <nseguralu@gmail.com>
2023-08-17 16:43:41 -0500

    ADD logo on header a fix the img source

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,7 @@
 import '../styles/Header.css';
 import { Person, Forum } from '@mui/icons-material';
 import { IconButton } from '@mui/material';
+import logo192 from '../assets/images/logo192.png';
 
 function Header() {
   return (
@@ -8,7 +9,7 @@ function Header() {
       <IconButton>
         <Person fontSize="large" className="header__icon" />
       </IconButton>
-      <img src="../assets/images/logo182.png" alt="header" />
+      <img className="header__logo" src={logo192} alt="header" />
       <IconButton>
         <Forum fontSize="large" className="header__icon" />
       </IconButton>
