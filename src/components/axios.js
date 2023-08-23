b09423a3aa117d925166eb8adc0a4e657c0d5a7f
Nkaleth <nseguralu@gmail.com>
2023-08-22 20:15:37 -0500

    ADD firebase deploy configuration

--- src/components/axios.js
+++ src/components/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const instance = axios.create({
-  baseURL: 'http://localhost:8001',
+  baseURL: 'https://dating-mern-be-728637c46f8e.herokuapp.com',
 });
 
 export default instance;
